e8f072656224063e2cff4c7efd115e194660adf4
Vinicius Eduardo Da Silva <vinicius.eduardo@senior.com.br>
2026-02-02 00:50:44 -0300

    V1.0.1 - Alteração Url API

--- axios.tsx
+++ axios.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const baseURL =
-  process.env.VITE_API_BASE_URL || process.env.EXPO_PUBLIC_API_BASE_URL || 'http://localhost:8080';
+const baseURL = process.env.EXPO_PUBLIC_API_BASE_URL ?? 'http://localhost:8080/api';
 
 export const API = axios.create({
   baseURL,
