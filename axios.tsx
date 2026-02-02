b112552d9010908b899bc40166bdb83890f16fde
Vinicius Eduardo Da Silva <vinicius.eduardo@senior.com.br>
2026-02-02 00:53:47 -0300

    V1.0.3 = Forcing PRD url

--- axios.tsx
+++ axios.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.EXPO_PUBLIC_API_BASE_URL ?? 'http://localhost:8080/api';
+const baseURL = 'https://api-storage-wivi.onrender.com/api';
 
 export const API = axios.create({
   baseURL,
